8c0d15fc859a6a8f7dbd32ea1630ad727202d17b
Hackbright Student <info@hackbrightacademy.com>
2015-09-29 12:38:01 -0700

    takes source file name from command line

--- markov.py
+++ markov.py
@@ -1,4 +1,5 @@
 from random import choice
+from sys import argv
 
 
 def open_and_read_file(file_path):
@@ -80,7 +81,7 @@ def make_text(chains):
     return text
 
 
-input_path = "green-eggs.txt"
+input_path = argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
